8fafae02697e43b137ae27d9b106b739c5e6367e
William Wennerström <william@wstrm.dev>
2020-07-31 18:11:41 +0200

    qutebrowser: better insert mode handling

--- qutebrowser/.config/qutebrowser/config.py
+++ qutebrowser/.config/qutebrowser/config.py
@@ -1,18 +1,40 @@
 config.source("theme-nord.py")
 
 c.auto_save.session = True
+
+c.content.autoplay = False
+
 c.downloads.location.directory = "~/downloads/"
+
 c.fonts.default_family = "Consolas"
 c.fonts.default_size = "13px"
+
 c.tabs.padding = {"bottom": 1, "left": 5, "right": 5, "top": 1}
+
+c.url.searchengines = {"DEFAULT": "https://google.com/search?q={}"}
+c.url.default_page = "about:blank"
+c.url.start_pages = "about:blank"
+
+c.input.insert_mode.auto_leave = (
+    False  # Do not leave insert mode if non-editable element is clicked.
+)
+c.input.insert_mode.auto_load = (
+    True  # Switch to insert mode if editable element is focused.
+)
+c.input.insert_mode.plugins = (
+    True  # Switch to insert mode when clicking plugin elements, e.g. Flash.
+)
 
 config.unbind("J", mode="normal")
 config.unbind("K", mode="normal")
+config.unbind("H", mode="normal")
+config.unbind("L", mode="normal")
 
 config.bind("H", "tab-prev")
 config.bind("L", "tab-next")
 
-config.bind("u", "back")
-config.bind("<Ctrl-R>", "forward")
+config.bind("J", "back")
+config.bind("K", "forward")
 
 config.bind("<Ctrl-v>", "spawn mpv {url}")
+config.bind("sc", "config-source")
